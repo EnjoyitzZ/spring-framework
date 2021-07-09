df4eefb450c381be104b430f8685722c441c4cf4
EnjoyitzZ <yjt201611@163.com>
2021-07-09 18:04:13 +0800

    使用@Import向容器中注入bean

--- spring-mytest/src/main/java/com/tong/entity/Person.java
+++ spring-mytest/src/main/java/com/tong/entity/Person.java
@@ -7,4 +7,34 @@ public class Person {
 	private String name;
 	private int age;
 	private double height;
+
+	public Person() {
+		System.out.println("Person 实例化");
+	}
+
+	public String getName() {
+		System.out.println("Person#getName");
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		System.out.println("Person#getAge");
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public double getHeight() {
+		return height;
+	}
+
+	public void setHeight(double height) {
+		this.height = height;
+	}
 }
--- spring-mytest/src/main/java/com/tong/entity/Student.java
+++ spring-mytest/src/main/java/com/tong/entity/Student.java
@@ -3,7 +3,43 @@ package com.tong.entity;
 import org.springframework.stereotype.Component;
 
 @Component
-public class Student {
+public class Student extends Person {
 	private long id;
 	private String name;
+
+	public Student() {
+		System.out.println("Student 实例化");
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	@Override
+	public String getName() {
+		System.out.println("Student#getName");
+		return name;
+	}
+
+	@Override
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void doSomething() {
+		System.out.println("this----------");
+		this.getName();
+		System.out.println("super---------");
+		super.getName();
+		super.getAge();
+	}
+
+	public static void main(String[] args) {
+		Student s = new Student();
+		s.doSomething();
+	}
 }
